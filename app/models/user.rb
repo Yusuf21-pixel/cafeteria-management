d8ac2ef4ae1b3d981033ce14a702ec2cc5ed5a58
yusuf <yusufdon001@gmail.com>
2021-06-17 00:32:48 +0530

    Validate sign-up form in embeded ruby file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ActiveRecord::Base
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, { presence: true, uniqueness: true }
   validates :password, presence: true
-  validates :address, presence: true
-  validates :phone_no, presence: true
   validates :role, presence: true
 
   has_secure_password
