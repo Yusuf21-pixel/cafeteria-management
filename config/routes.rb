bfef3092fa7c3d332bce3d0f2491cb659b190e57
yusuf <yusufdon001@gmail.com>
2021-06-16 21:28:42 +0530

    Create front-end for sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/" => "sessions#new"
+  root to: "sessions#new"
   resources :users
   post "/signin" => "sessions#create", as: :sessions
   delete "/signout" => "sessions#destroy", as: :destroy_session
