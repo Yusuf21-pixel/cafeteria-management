b3a2c387c7657b02136908ab79e98e9cad06680f
yusuf <yusufdon001@gmail.com>
2021-06-22 00:12:38 +0530

    Add navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "sessions#new"
-  resources :users
+  resources :users, :menu_categories
   post "/signin" => "sessions#create", as: :sessions
   delete "/signout" => "sessions#destroy", as: :destroy_session
 end
