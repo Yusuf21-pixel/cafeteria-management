9205d4b2f4bd857872bb3bb32c7d069b5c487878
yusuf <yusufdon001@gmail.com>
2021-06-25 01:46:30 +0530

    Add create method and create a view to render menu item view

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,13 +1,18 @@
 class MenuItemsController < ApplicationController
   def create
-    item = MenuItem.new(name: params[:name],
+    item = MenuItem.new(name: params[:name].capitalize(),
                         price: params[:price],
-                        description: params[:description],
+                        description: params[:description].capitalize(),
                         menu_category_id: params[:category_id],
                         status: params[:status])
     unless item.save
       flash[:error] = item.errors.full_messages.join(", ")
     end
     redirect_to menu_categories_path
+  end
+
+  def render_items
+    @menu_category = MenuCategory.all
+    @id = params[:id]
   end
 end
