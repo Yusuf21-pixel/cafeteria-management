d35afd3296995d4d99a17f10698e91dd03b7442c
yusuf <yusufdon001@gmail.com>
2021-06-25 19:00:45 +0530

    Add update method to menu items controller

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -14,5 +14,26 @@ class MenuItemsController < ApplicationController
   def render_items
     @menu_category = MenuCategory.all
     @id = params[:id]
+    if @id == 0 && (!@menu_category.empty?)
+      @category = MenuCategory.first
+    else
+      @category = MenuCategory.find(@id)
+    end
+    @menu_items = @category.menu_items
+  end
+
+  def update_view
+    @id = params[:id]
+    @item = MenuItem.find(@id)
+  end
+
+  def update
+    item = MenuItem.find(params[:id])
+    item.status = params[:status]
+    item.name = params[:item_name].capitalize()
+    item.price = params[:price]
+    item.description = params[:description].capitalize()
+    item.save!
+    redirect_to menu_items_view_path(id: item.menu_category_id)
   end
 end
