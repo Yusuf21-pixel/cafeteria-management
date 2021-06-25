e8c7c82da1eb0bc283d8eacd73a59361221112c8
yusuf <yusufdon001@gmail.com>
2021-06-25 19:04:52 +0530

    Display Update view for menu items

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get "/update_menucategory_view/:id" => "menu_categories#update_view", as: :update_category_view
   get "/add_item_view/:id" => "menu_categories#add_item_view", as: :add_item_view
   get "/menuitem_view/:id" => "menu_items#render_items", as: :menu_items_view
+  get "/update_menuitem_view/:id" => "menu_items#update_view", as: :update_item_view
 end
