2325a1f503c205b3c8c960f250bf6a9d7300be71
yusuf <yusufdon001@gmail.com>
2021-06-28 00:31:59 +0530

    Add destroy method to menu items controller

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -14,12 +14,13 @@ class MenuItemsController < ApplicationController
   def render_items
     @menu_category = MenuCategory.all
     @id = params[:id]
-    if @id == 0 && (!@menu_category.empty?)
+    if @id == "0"
       @category = MenuCategory.first
+      redirect_to menu_items_view_path(id: @category.id) unless @category.nil?
     else
       @category = MenuCategory.find(@id)
     end
-    @menu_items = @category.menu_items
+    @menu_items = @category.menu_items unless @category.nil?
   end
 
   def update_view
@@ -35,5 +36,17 @@ class MenuItemsController < ApplicationController
     item.description = params[:description].capitalize()
     item.save!
     redirect_to menu_items_view_path(id: item.menu_category_id)
+  end
+
+  def destroy
+    item = MenuItem.find(params[:id])
+    category_id = item.menu_category_id
+    item.destroy
+    redirect_to menu_items_view_path(category_id)
+  end
+
+  def add_item_view
+    @id = params[:id]
+    @category_name = MenuCategory.find(@id).name
   end
 end
