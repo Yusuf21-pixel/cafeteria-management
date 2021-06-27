6951b07af474c4e963ef7ef37ff15bb6d0184df4
yusuf <yusufdon001@gmail.com>
2021-06-28 00:34:24 +0530

    Add text color to navbar, Add image to display no items found

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   get "/add_item_view/:id" => "menu_categories#add_item_view", as: :add_item_view
   get "/menuitem_view/:id" => "menu_items#render_items", as: :menu_items_view
   get "/update_menuitem_view/:id" => "menu_items#update_view", as: :update_item_view
+  post "/create_menu_category" => "menu_categories#create_category", as: :create_menu_category
 end
