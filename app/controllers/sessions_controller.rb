f6f3ba415207f323c9c71ae2e53b08586fc99eb1
yusuf <yusufdon001@gmail.com>
2021-06-28 01:08:19 +0530

    Store user id as cookie when signed in and create destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,16 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
+      session[:current_user_id] = user.id
       render plain: "true"
     else
-      render plain: "false"
+      redirect_to sessions_path
     end
+  end
+
+  def destroy
+    session[:current_user_id] = nil
+    @current_user = nil
+    redirect_to root_path
   end
 end
