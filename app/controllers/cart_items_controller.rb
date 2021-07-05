de7173c7d0d4ebd675afab4d4e407daab0444131
yusuf <yusufdon001@gmail.com>
2021-07-05 17:00:09 +0530

    Add functionalities to cart_items Create, update and destroy

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,56 @@
+class CartItemsController < ApplicationController
+  def index
+  end
+
+  def create
+    cart = Cart.find_by(user_id: @current_user.id)
+    unless (cart.cart_items.find_by(menu_item_id: params[:id]))
+      menu_item = MenuItem.find(params[:id])
+      cart_item = CartItem.new(cart_id: cart.id,
+                               menu_item_id: menu_item.id,
+                               menu_item_name: menu_item.name,
+                               menu_item_price: menu_item.price,
+                               quantity: 1)
+      unless (cart_item.save)
+        flash[:error] = "Something went wrong"
+      end
+    end
+    redirect_to users_menu_path(id: 0)
+  end
+
+  def add_quantity
+    cart = Cart.find_by(user_id: @current_user.id)
+    cart_item = cart.cart_items.find(params[:id])
+    cart_item.quantity += 1
+    cart_item.save!
+    redirect_to users_menu_path(id: 0), status: 303
+  end
+
+  def reduct_quantity
+    cart = Cart.find_by(user_id: @current_user.id)
+    cart_item = cart.cart_items.find(params[:id])
+    unless (cart_item.quantity == 1)
+      cart_item.quantity -= 1
+      cart_item.save!
+    end
+    redirect_to users_menu_path(id: 0), status: 303
+  end
+
+  def delete_all
+    cart = Cart.find_by(user_id: @current_user.id)
+    cart_items = cart.cart_items
+    cart_items.destroy_all
+    redirect_to users_menu_path(id: 0), status: 303
+  end
+
+  def destroy
+    cart = Cart.find_by(user_id: @current_user.id)
+    cart_item = cart.cart_items.find(params[:id])
+    cart_item.destroy
+    redirect_to users_menu_path(id: 0), status: 303
+  end
+
+  def checkout
+    render plain: "hello"
+  end
+end
