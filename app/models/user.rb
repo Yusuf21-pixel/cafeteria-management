6ff3e7352616e4c6ec80c847cda6618b6cef5ff8
yusuf <yusufdon001@gmail.com>
2021-07-05 16:40:14 +0530

    Add models to tables that are created

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   validates :email, { presence: true, uniqueness: true }
   validates :password, presence: true
   validates :role, presence: true
-
+  has_many :addresses
   has_secure_password
 end
