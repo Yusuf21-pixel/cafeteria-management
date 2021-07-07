bac3eb74a1a8f234ca5a61226f861f91450890a6
yusuf <yusufdon001@gmail.com>
2021-07-08 00:44:15 +0530

    Add functionalities report, create owner and update owner

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,38 @@
 class AdminController < ApplicationController
   def index
   end
+
+  def reports
+  end
+
+  def users_profile
+    @users = User.all.order(:id)
+  end
+
+  def update_special_users_view
+    @id = params[:id]
+    @user = User.find(@id)
+  end
+
+  def edit_special_users
+    user = User.find(params[:id])
+    user.name = params[:name]
+    user.phone_no = params[:phone_no]
+    user.email = params[:email]
+    user.save!
+    redirect_to users_profile_path
+  end
+
+  def invoices
+    @orders = Order.all
+    unless params[:first_date].nil?
+      @orders = Order.date_range(params[:first_date].to_date, params[:last_date].to_date)
+    end
+  end
+
+  def customers_profile_view
+  end
+
+  def customer_report
+  end
 end
